3daf96a5ba3810677956c6d3a9e46ad8aa50e677
maniezhilan <maniezhilan@gmail.com>
2017-08-15 22:25:53 -0500

    Questions added to form

--- app/surveys/models.py
+++ app/surveys/models.py
@@ -1,6 +1,7 @@
 from marshmallow_jsonapi import Schema, fields
 from marshmallow import validate
 from app.basemodels import db, CRUD_MixIn
+from app.questions.models import Questions, QuestionsSchema
 
 
 class Surveys(db.Model, CRUD_MixIn):
@@ -13,6 +14,9 @@ class Surveys(db.Model, CRUD_MixIn):
         db.TIMESTAMP, server_default=db.func.current_timestamp())
     modification_time = db.Column(
         db.TIMESTAMP, server_default=db.func.current_timestamp())
+    # Relationships
+    questions = db.relationship('Questions', backref='surveys',
+                                lazy='dynamic')
 
     def __init__(self,  title,  description,  active,  creation_time,  modification_time,  question, ):
 
@@ -33,7 +37,9 @@ class SurveysSchema(Schema):
     title = fields.String(validate=not_blank)
     description = fields.String(validate=not_blank)
     active = fields.Integer(required=True)
-
+    #If the field is a collection of nested objects, you must set many=True.
+    #http://marshmallow.readthedocs.io/en/latest/nesting.html#nesting
+    questions = fields.Nested(QuestionsSchema, many=True)
 
 
     # self links
